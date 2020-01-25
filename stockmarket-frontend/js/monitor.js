53221e6dea0747d59de38a71bc1bda67a05b6696
deepcloudlabs <34dmng65jm>
2020-01-25 12:11:41 +0300

    format source code minor improvements

--- stockmarket-frontend/js/monitor.js
+++ stockmarket-frontend/js/monitor.js
@@ -1,10 +1,11 @@
 class DashboardViewModel {
-    constructor(){
+    constructor() {
         // domain related observables
-        this.currencies= ko.observableArray([]);
-        this.symbol= ko.observable("");
-        this.isMonitoring= ko.observable(false);
-        this.connected= ko.observable(false);
+        this.currencies = ko.observableArray([]);
+        this.symbol = ko.observable("GARAN");
+        this.symbols = ko.observableArray(["AKBNK", "ASELS", "GARAN", "TCELL"]);
+        this.isMonitoring = ko.observable(false);
+        this.connected = ko.observable(false);
         this.windowSize = ko.observable(10);
         this.data = {
             labels: ko.observableArray([]),
@@ -20,34 +21,26 @@ class DashboardViewModel {
                     data: ko.observableArray([])
                 }
             ]
-        } ;
-
-        this.changeLng= this.changeLng.bind(this);
-        this.i18n= this.i18n.bind(this);
-        this.translate= this.translate.bind(this);
-        this.start= this.start.bind(this);
-        this.stop= this.stop.bind(this);
-        this.retrieveCurrencies= this.retrieveCurrencies.bind(this);
+        };
 
         // connect to the websocket server
         this.socket = new SockJS("http://localhost:8001/market/api/v1/changes");
         this.stompClient = Stomp.over(this.socket);
-        this.stompClient.debug = () => {}
-        this.stompClient.connect({},(frame)=>{
+        this.stompClient.debug = () => {
+        }
+        this.stompClient.connect({}, (frame) => {
             toastr.success("Connected!");
             this.connected(true);
             this.stompClient.subscribe("/topic/changes",
-                (msg)=>{
-                    if(!this.isMonitoring()) return;
+                (msg) => {
+                    if (!this.isMonitoring()) return;
                     let trade = JSON.parse(msg.body);
-                    this.data.datasets[0]
-                             .data
-                             .push(Number(trade.price));
+                    this.data.datasets[0].data.push(Number(trade.price));
                     let now = new Date().toTimeString();
                     now = now.replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1');
                     this.data.labels.push(now);
-                    if (this.data.datasets[0].data().length>this.windowSize()){
-                        let sliceIndex = this.data.datasets[0].data().length-this.windowSize();
+                    if (this.data.datasets[0].data().length > this.windowSize()) {
+                        let sliceIndex = this.data.datasets[0].data().length - this.windowSize();
                         let slicedData = this.data.datasets[0].data.slice(sliceIndex);
                         this.data.datasets[0].data(slicedData);
                         let slicedLabels = this.data.labels.slice(sliceIndex);
@@ -58,51 +51,35 @@ class DashboardViewModel {
     }
 
     // i18n
-    changeLng(lng){
-        i18n.setLng(lng,() => {
-            this.i18n();
-        });
+    changeLng = (lng) => {
+        i18n.setLng(lng, () => this.i18n());
     };
 
-    i18n(){
+    i18n = () => {
         $(document).i18n();
     };
 
-    translate(word){
-        return i18n.t(word);
-    };
-
-    retrieveCurrencies(){
-        $.ajax({
-            method: "GET",
-            url: AppConfig.REST_API_BASE_URL.concat("/currencies"),
-            cache: false,
-            success: (currencies) => {
-                 /* TODO : update the view model! */
-                 toastr.success(i18n.t("messageCurrenciesRetrieved"), "", AppConfig.TOASTR_CONFIG);
-            },
-             error: ajaxErrorHandler
-        });
-    };
+    translate = (word) => i18n.t(word) ;
 
     // starts monitoring
-    start(){
+    start = () => {
         this.isMonitoring(true);
+        this.data.datasets[0].label = this.symbol();
         toastr.success(i18n.t("messageMonitoringStarted"), "", AppConfig.TOASTR_CONFIG);
     };
 
     // stops monitoring
-    stop(){
+    stop = () => {
         this.isMonitoring(false);
         toastr.warning(i18n.t("messageMonitoringStoped"), "", AppConfig.TOASTR_CONFIG);
     };
 
 };
 
-var dashBoardViewModel= new DashboardViewModel();
+var dashBoardViewModel = new DashboardViewModel();
 
-$( () => {
-    i18n.init(AppConfig.I18N_CONFIG,(t) => {
+$(() => {
+    i18n.init(AppConfig.I18N_CONFIG, (t) => {
         $(document).i18n();
         ko.applyBindings(dashBoardViewModel);
     });
